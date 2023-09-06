086fa7f17cb9f45d23e845f6857ad623d32dc229
Dima <deomin.dmitriy@gmail.com>
2023-09-06 21:11:02 +0300

    -

---
+++ src/data.rs
@@ -0,0 +1,12 @@
+pub fn get_text_text_info()->String{
+    let test_info = "Для теста добавьте один или несколько из этих адресов
+в режиме измерения скорости(0 количесто ядер процессора)
+после 5 секунд после запуска подставляеться тестовый секретный ключ
+
+
+14NWDXkQwcGN1Pd9fboL8npVynD5SfyJAE
+1JRW4d8vHZseMEtYbgJ7MwPG1TasHUUVNq
+3LtPxQEqqo1sYGF7VTEufzBUki6pFiDVa1
+bc1qhuwxrtqe2akhr4rz8vv97waw9g75ma4unk5vnf";
+    test_info.to_string()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+mod data;
+
 extern crate bitcoin;
 extern crate secp256k1;
 extern crate num_cpus;
@@ -13,6 +15,7 @@ use std::{
     io::{BufRead, BufReader},
     path::Path,
 };
+use std::str::FromStr;
 
 use secp256k1::{rand, Secp256k1, SecretKey};
 use bitcoin::{network::constants::Network, PrivateKey, Address, PublicKey};
@@ -36,6 +39,7 @@ async fn main() {
             println!("Параметры не найдены , создание и установка в режим измерения скорости\n");
             let t = format!("0 -Количество ядер от 0 до {} (0 - режим измерения скорости)", num_cpus::get());
             add_v_file(&file_cong, &t);
+            add_v_file("test.txt",data::get_text_text_info().as_str());
             vec![t.to_string()]
         }
     };
@@ -44,8 +48,8 @@ async fn main() {
     let stroka_0_all = &conf[0].to_string();
     let mut num_cores: u8 = first_word(stroka_0_all).to_string().parse::<u8>().unwrap();
 
-    println!("Поиск ведется по адресу(1...) старому и новому");
-    println!("База ваших адресов должна лежать рядом в bip44_wallets.txt, иначе загрузиться встроеная\n");
+    println!("Поиск ведется по BTC адресам bip44(uncompressed,compressed),bip49,bip84 ");
+    println!("База ваших адресов должна лежать рядом в all_wallets.txt, иначе загрузиться встроеная\n");
 
     println!("Если чудо произойдёт выведется результат в консоль и ключ запишеться BOBLO.txt\n\
     (будет лежать рядом, создавать не обязательно)\n");
@@ -61,18 +65,19 @@ async fn main() {
     println!("Из {} логических процессоров задействовано:{} \n", num_cpus::get(), num_cores);
 
 
-    let file_content = match lines_from_file("bip44_wallets.txt") {
+    let file_content = match lines_from_file("all_wallets.txt") {
         Ok(file) => {
             println!("Адресов в файле: {}", file.len());
             file
         }
         Err(_) => {
-            println!("bip44_wallets.txt не найден , загружаю встроенный список");
-            let dockerfile = include_str!("bip44_wallets.txt");
+            println!("all_wallets.txt не найден , загружаю встроенный список");
+            let dockerfile = include_str!("all_wallets.txt");
             let mut vs: Vec<String> = vec![];
             for c in dockerfile.split("\n") {
                 vs.push(c.to_string());
             }
+            add_v_file("all_wallets.txt",dockerfile);
             vs
         }
     };
@@ -100,35 +105,59 @@ async fn main() {
 fn process(file_content: &RwLockReadGuard<HashSet<String>>, bench: bool) {
     let mut start = Instant::now();
     let mut speed: u32 = 0;
-
+    let mut test_find = false;
+    let mut time_test = 0;
     loop {
-        let secret_key = SecretKey::new(&mut rand::thread_rng());
-        let private_key_uncompressed = PrivateKey::new_uncompressed(secret_key, Network::Bitcoin);
-        let public_key_uncompressed = PublicKey::from_private_key(&Secp256k1::new(), &private_key_uncompressed);
-        let address_uncompressed = Address::p2pkh(&public_key_uncompressed, Network::Bitcoin);
+        let secret_key = match test_find {
+            true => { test_find=false;
+                SecretKey::from_str("23d4a09295be678b21a5f1dceae1f634a69c1b41775f680ebf8165266471401b").expect("") }
+            false => {SecretKey::new(&mut rand::thread_rng())}
+        };
 
-        let private_key_compressed = PrivateKey::new(secret_key, Network::Bitcoin);
-        let public_key_compressed = PublicKey::from_private_key(&Secp256k1::new(), &private_key_compressed);
-        let address_compressed = Address::p2pkh(&public_key_compressed, Network::Bitcoin);
+        let private_key_u = PrivateKey::new_uncompressed(secret_key, Network::Bitcoin);
+        let public_key_u = PublicKey::from_private_key(&Secp256k1::new(), &private_key_u);
+        let addres_u = Address::p2pkh(&public_key_u, Network::Bitcoin);
 
+        let private_key_c = PrivateKey::new(secret_key, Network::Bitcoin);
+        let public_key_c = PublicKey::from_private_key(&Secp256k1::new(), &private_key_c);
+        let addres_c = Address::p2pkh(&public_key_c, Network::Bitcoin);
 
-        if file_content.contains(&address_uncompressed.to_string()) {
-            print_and_save(address_uncompressed.to_string(), private_key_uncompressed.to_string(), secret_key.display_secret().to_string());
+        let addres_49 = Address::p2shwpkh(&public_key_c,Network::Bitcoin).expect("p2shwpkh");
+        let addres_84 = Address::p2wpkh(&public_key_c,Network::Bitcoin).expect("p2wpkh");
+
+        if file_content.contains(&addres_u.to_string()) {
+            print_and_save(addres_u.to_string(), private_key_u.to_string(), secret_key.display_secret().to_string());
         }
-        if file_content.contains(&address_compressed.to_string()) {
-            print_and_save(address_compressed.to_string(), private_key_compressed.to_string(), secret_key.display_secret().to_string());
+        if file_content.contains(&addres_c.to_string()) {
+            print_and_save(addres_c.to_string(), private_key_c.to_string(), secret_key.display_secret().to_string());
         }
+        if file_content.contains(&addres_49.to_string()) {
+            print_and_save(addres_49.to_string(), private_key_c.to_string(), secret_key.display_secret().to_string());
+        }
+        if file_content.contains(&addres_84.to_string()) {
+            print_and_save(addres_84.to_string(), private_key_c.to_string(), secret_key.display_secret().to_string());
+        }
+
+        //после ~5 секунд включим тест
+        if time_test==10000 { test_find = true; }
 
         if bench {
             speed = speed + 1;
+            time_test = time_test+1;
             if start.elapsed() >= Duration::from_secs(1) {
-                let address = Address::p2pkh(&public_key_uncompressed, Network::Bitcoin);
                 println!("--------------------------------------------------------");
                 println!("Проверил {:?} комбинаций за 1 сек ", speed);
                 println!("Вариант подбора:");
-                println!("Adress:{}\nPublicKey:{}\nPrivateKey:{}", address, &public_key_uncompressed, &private_key_uncompressed);
-                println!("ADRESS:{}\nPublicKeyc:{}\nPrivateKey:{}", address_compressed,private_key_compressed,public_key_compressed);
+                println!("SecretKey:{}",&secret_key.display_secret());
+                println!("uncompressed");
+                println!("Adress_32:{}\nPublicKey:{}\nPrivateKey:{}", &addres_u, &public_key_u, &private_key_u);
+                println!("compressed");
+                println!("PublicKey:{}\nPrivateKey:{}",&public_key_c,&private_key_c);
+                println!("Adress_44:{}", &addres_c);
+                println!("Adress_49:{}", &addres_49);
+                println!("Adress_84:{}", &addres_84);
                 println!("--------------------------------------------------------");
+
                 start = Instant::now();
                 speed = 0;
             }
@@ -137,12 +166,16 @@ fn process(file_content: &RwLockReadGuard<HashSet<String>>, bench: bool) {
 }
 
 fn print_and_save(adress: String, key: String, secret_key: String) {
+    println!("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
     println!("ADRESS:{}", &adress);
     println!("PrivateKey:{}", &key);
     println!("Secret_key:{}", &secret_key);
-    let s = format!("ADRESS:{} PrivateKey:{}\nSecret_key{}\n", &adress, &key, &secret_key);
+    let s = format!("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\
+    ADRESS:{adress}\nPrivateKey:{key}\nSecret_key:{secret_key}\n\
+    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n");
     add_v_file("BOBLO.txt", &s);
-    println!("-----------\n-----------\nСохранено в BOBLO.txt-----------\n-----------\n");
+    println!("----------------------\nСохранено в BOBLO.txt\n----------------------");
+    println!("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
 }
 
 fn lines_from_file(filename: impl AsRef<Path>) -> io::Result<Vec<String>> {
